3e9afd5403a1a44672689441b299cd7373bcc885
niko-roether <nicholas.roether@t-online.de>
2024-11-04 22:24:23 +0100

    Fix ingredient conditions

---
+++ data/origins-plus-plus/fixIngredientCondition.mjs
@@ -0,0 +1,48 @@
+import fs from "node:fs";
+
+const paths = fs.globSync("./**/*.json");
+
+function isProbablyIngredientCondition(obj) {
+	if (typeof obj !== "object") return false;
+	return obj.type === "origins:ingredient";
+}
+
+function fixIngredientCondition(obj) {
+	if (typeof obj !== "object") return false;
+	let fixed = false;
+	if ("id" in obj.ingredient) {
+		obj.ingredient.item = obj.ingredient.id;
+		obj.ingredient.id = undefined;
+		fixed = true;
+	}
+	return fixed;
+}
+
+function fixRecursive(obj) {
+	if (typeof obj !== "object") return false;
+
+	if (isProbablyIngredientCondition(obj)) {
+		return fixIngredientCondition(obj);
+	}
+
+	let fixed = false;
+	if (Array.isArray(obj)) {
+		for (const item of obj) {
+			fixed ||= fixRecursive(item);
+		}
+		return fixed;
+	}
+
+
+	for (const key in obj) {
+		fixed ||= fixRecursive(obj[key]);
+	}
+	return fixed;
+}
+
+for (const path of paths) {
+	const data = JSON.parse(fs.readFileSync(path));
+	if (fixRecursive(data)) {
+		fs.writeFileSync(path, JSON.stringify(data, null, 2));
+	}
+}
