6dae40233721c9b421fea1363047f5807ebb9d1f
niko-roether <nicholas.roether@t-online.de>
2024-11-05 14:12:01 +0100

    Revert "Fix item conditions with nbt data, attempt 7"
    This reverts commit 2519c0111a6e84e3c6d8d3b754f4a532e3be3220.

--- data/origins-plus-plus/fixItemNbtCondition.mjs
+++ data/origins-plus-plus/fixItemNbtCondition.mjs
@@ -4,14 +4,14 @@ const paths = fs.globSync("./**/*.json");
 
 function isProbablyIngredientCondition(obj) {
 	if (typeof obj !== "object") return false;
-	return (obj.type === "origins:nbt" || obj.type === "origins:custom_data" || obj.type === "minecraft:custom_data" || obj.type === "apoli:custom_data" || obj.type === "custom_data" || obj.type === "apoli:nbt" || obj.type === "apoli::nbt") && "nbt" in obj;
+	return (obj.type === "origins:nbt" || obj.type === "origins:custom_data" || obj.type === "minecraft:custom_data" || obj.type === "apoli:custom_data" || obj.type === "custom_data" || obj.type === "apoli:nbt") && "nbt" in obj;
 }
 
 function fixIngredientCondition(obj) {
 	if (typeof obj !== "object") return false;
 	let fixed = false;
-	if (obj.type !== "apoli::nbt") {
-		obj.type = "apoli::nbt";
+	if (obj.type !== "apoli:nbt") {
+		obj.type = "apoli:nbt";
 		fixed = true;
 	}
 	return fixed;
