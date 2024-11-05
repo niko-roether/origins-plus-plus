d3d3513e5504fca9c726670ff2a8f6acaef1572b
niko-roether <nicholas.roether@t-online.de>
2024-11-05 14:28:49 +0100

    Fix item conditions (for real this time)

---
+++ data/origins-plus-plus/fixItemCondition.mjs
@@ -0,0 +1,49 @@
+import fs from "node:fs";
+
+const paths = fs.globSync("./**/*.json");
+
+function isProbablyItemCondition(obj, key) {
+	if (typeof obj[key] !== "object") return false;
+	if (key !== "item_condition") return false;
+	return true;
+}
+
+function fixItemCondition(obj) {
+	let fixed = false;
+	switch (obj.type) {
+		case "origins:nbt":
+			obj.type = "origins:custom_data";
+			fixed = true;
+			break;
+	}
+	return fixed;
+}
+
+function fixRecursive(obj) {
+	if (typeof obj !== "object") return false;
+
+	let fixed = false;
+	if (Array.isArray(obj)) {
+		for (const item of obj) {
+			fixed ||= fixRecursive(item);
+		}
+		return fixed;
+	}
+
+
+	for (const key in obj) {
+		if (isProbablyItemCondition(obj, key)) {
+			fixed ||= fixItemCondition(obj[key]);
+			continue;
+		}
+		fixed ||= fixRecursive(obj[key]);
+	}
+	return fixed;
+}
+
+for (const path of paths) {
+	const data = JSON.parse(fs.readFileSync(path));
+	if (fixRecursive(data)) {
+		fs.writeFileSync(path, JSON.stringify(data, null, 2));
+	}
+}
