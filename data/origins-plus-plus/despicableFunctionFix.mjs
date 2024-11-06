6c8acef0917187f724cd9b9fb031dabd8724be95
niko-roether <nicholas.roether@t-online.de>
2024-11-06 12:56:22 +0100

    Despicable fix for functions part 1: add parameters to all function calls

---
+++ data/origins-plus-plus/despicableFunctionFix.mjs
@@ -0,0 +1,41 @@
+import fs from "node:fs";
+
+const paths = fs.globSync("./**/*.json");
+
+function despicableFunctionFix(cmd) {
+	if (typeof cmd !== "string") return null;
+	const result = /^function ([^ ]+)/.exec(cmd);
+	if (!result) return null;
+	return `function ${result[1]} {power:"power",resource:"resource"}`
+}
+
+function fixRecursive(obj) {
+	if (typeof obj !== "object") return false;
+
+	let fixed = false;
+	if (Array.isArray(obj)) {
+		for (const item of obj) {
+			fixed = fixRecursive(item) || fixed;
+		}
+		return fixed;
+	}
+
+
+	for (const key in obj) {
+		const fix = despicableFunctionFix(obj[key]);
+		if (fix) {
+			obj[key] = fix;
+			fixed = true;
+		} else {
+			fixed = fixRecursive(obj[key]) || fixed;
+		}
+	}
+	return fixed;
+}
+
+for (const path of paths) {
+	const data = JSON.parse(fs.readFileSync(path));
+	if (fixRecursive(data)) {
+		fs.writeFileSync(path, JSON.stringify(data, null, 2));
+	}
+}
